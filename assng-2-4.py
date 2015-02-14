6a510ceeb1145f56d62f1720dfe9762901e0ce57
Rabutogrey <rabutogrey@gmail.com>
2015-02-13 23:10:12 -0500

    Book Exercises

---
+++ assng-2-4.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+# This program prints the value of the expressions and type of their values
+
+width = 17
+height = 12.0
+
+expr1 = width/2
+expr2 = width/2.0
+expr3 = height/3
+expr4 = 1 + 2 * 5
+
+print "Expression width/2 is ", expr1 ,"and type is", type(expr1)
+print "Expression width/2.0 is ", expr2 ,"and type is", type(expr2)
+print "Expression height/2 is ", expr3 ,"and type is", type(expr3)
+print "Expression 1 + 2 * 5 is ", expr4 ,"and type is", type(expr4)
+
+
+
+
---
+++ assng-2-5.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+# This program converts Celsius to Fahrenheit
+
+c = float(raw_input("Please, enter the temperature in Celsius: "))
+f = (c * 9 / 5) + 32
+
+print c, "C is", f ,"F"
+
+
