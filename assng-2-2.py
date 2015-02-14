a414b03ac4f153b25a02ee9fbbbb7369cfb1eb3e
Rabutogrey <rabutogrey@gmail.com>
2015-02-13 23:00:59 -0500

    Adding files

---
+++ assng-2-2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+# This program prompt a user for their name and then
+# welcomes them.
+
+name = raw_input("What is your name: ")
+print "Hello", name
+
---
+++ assng-2-3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+# This program prompts the user for hours and rate per hour using raw_input
+# to compute gross pay.
+
+hours = float(raw_input("How many hours............: "))
+rate = float(raw_input("What is the rate per hour.: "))
+
+print "Gross pay is", hours * rate
+
